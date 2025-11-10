27b095f470834afecb39baf4a5cab42f14502c90
ROG4113 <ass0009916@gmail.com>
2025-11-10 23:50:55 +0530

    test context

---
+++ src/main/java/com/springframework/spring6di/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.springframework.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("I am in the Controller");
+        return  "Hello Everyone!!";
+    }
+}
--- src/test/java/com/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,40 @@
 package com.springframework.spring6di;
 
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.springframework.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	private static final Logger log=LoggerFactory.getLogger(Spring6DiApplicationTests.class);
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void textGetController(){
+		System.out.println(myController.sayHello());
+
+		log.info(myController.sayHello());
+	}
+
+	@Test
+	void testAutowireOfControllerFromCtx(){
+		MyController myController=applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+		log.info(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
